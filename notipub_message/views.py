2a5690720a0c842a68a78d9bc877d24a0d899b71
harry81 <pointer81@gmail.com>
2014-07-12 10:04:28 +0900

    Show default page on CMS

--- notipub_message/views.py
+++ notipub_message/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 
 # Create your views here.
+def home(request,
+         template="notipub_message/index.html"):
+    context = {}
+    return render_to_response(template,
+                                context, context_instance=RequestContext(request))
