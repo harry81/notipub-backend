70ba00d7348919c99a9ea4f40d7dbd43625add3a
harry81 <pointer81@gmail.com>
2014-07-13 17:09:28 +0900

    Send token to the registered device

--- notipub_message/management/commands/sendmessage.py
+++ notipub_message/management/commands/sendmessage.py
@@ -1,20 +1,22 @@
+# encoding: utf-8
+
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from gcm import GCM
 
+from notipub_message.models import DeviceToken
 
-
-reg_id="APA91bGm4KBvvDdecHnp1vzLdFA3xxW8am1WZTxX0-Qwf6vUqpvhMS0pMCqX0TSJuocmaDPT7kQKTh2VU0eR0o9B0ZxECCHkpFoz97KD6K-vxxLBgQN4vROZL2QUG32qsf06XnM-3j36p3ZiX4GT7sri5qJu-fjfrw"
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write("hello", ending='')
         gcm = GCM(settings.GOOGLE_API_KEY)
-        payload = {}
-        payload['message']="this is a message."
-        payload['msgcnt']="4"
-        payload['timeStamp']="2014 07 03 20 20 00."
-        gcm.plaintext_request(registration_id=reg_id, data=payload)
 
+        for token in set(DeviceToken.objects.all().values_list('token', flat=True)):
+            payload = {}
+            payload['message']=u"서울은 맑음"
+            payload['msgcnt']="3"
+            payload['timeStamp']="2014 07 03 20 20 00."
+            print 'Sented to [%s].' % token
+            gcm.plaintext_request(registration_id=reg_id, data=payload)
     
 
