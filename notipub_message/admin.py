e5cfebd7cf6aabbc90391118f572eb8ff1c3ec5c
harry81 <pointer81@gmail.com>
2014-07-13 13:26:59 +0900

    set list order in token admin

--- notipub_message/admin.py
+++ notipub_message/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from notipub_message.models import DeviceToken
 
 class DeviceTokenAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'token', 'created_at')
     ordering = ['-created_at']
 
 admin.site.register(DeviceToken ,DeviceTokenAdmin)
