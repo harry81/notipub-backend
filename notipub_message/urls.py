512d74a7924c5029a20421393b154e757028674f
harry81 <pointer81@gmail.com>
2014-07-13 10:59:26 +0900

    WIP

--- notipub_message/urls.py
+++ notipub_message/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import *
 
 urlpatterns = patterns('notipub_message.views',
     url(r'^notipub$', 'home', name='home'),
-    url(r'^register_token$', 'register_token', name='register_token'),
+    url(r'^notipub/register_token$', 'register_token', name='register_token'),
 )
