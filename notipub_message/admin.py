65697774e399188e0746c5027fa95ca7186e1e61
harry81 <pointer81@gmail.com>
2014-07-13 14:29:17 +0900

    remove en language in urls

--- notipub_message/admin.py
+++ notipub_message/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from notipub_message.models import DeviceToken
 
 class DeviceTokenAdmin(admin.ModelAdmin):
-    list_display = ('uuid', 'token', 'created_at')
+    list_display = ('uuid', 'token', 'created_at', 'lat', 'lng')
     ordering = ['-created_at']
 
 admin.site.register(DeviceToken ,DeviceTokenAdmin)
