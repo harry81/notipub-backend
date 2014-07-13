7ddf5d75df137bd1edede04569b8245cd998053a
harry81 <pointer81@gmail.com>
2014-07-13 18:04:40 +0900

    WIP send token

--- notipub_message/management/commands/sendmessage.py
+++ notipub_message/management/commands/sendmessage.py
@@ -2,7 +2,7 @@
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-from gcm import GCM, GCMNotRegisteredException
+from gcm import GCM
 
 from notipub_message.models import DeviceToken
 
@@ -18,12 +18,13 @@ class Command(BaseCommand):
             payload['timeStamp']="2014 07 03 20 20 00."
             try:
                 canonical_id = gcm.plaintext_request(registration_id=reg_id, data=payload)
-                token = DeviceToken.objects.filter(registration_id=reg_id)
-                token.registration_id = canonical_id
-                token.save()
-                self.stdout.write('Sent to [%s].' % reg_id, ending='')
-            except GCMNotRegisteredException:
-                token = DeviceToken.objects.filter(registration_id=reg_id)
+                if canonical_id:
+                    token = DeviceToken.objects.filter(token=reg_id)
+                    token.token = canonical_id
+                    token.save()
+                    self.stdout.write('Sent to [%s].' % reg_id, ending='')
+            except :
+                token = DeviceToken.objects.filter(token=reg_id)
                 token.delete()
                 self.stdout.write('Deleted  [%s].' % reg_id, ending='')
 
